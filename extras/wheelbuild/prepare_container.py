fc8cb2339cee84c9200b969d6596acede4da71fe
Robin De Schepper <robin.deschepper93@gmail.com>
2022-10-11 16:46:36 +0200

    no more need for skrepo

--- extras/wheelbuild/prepare_container.py
+++ extras/wheelbuild/prepare_container.py
@@ -6,9 +6,6 @@ import os
 # if we use .py files instead of .sh files, we can shell out from here with a correct
 # keyboard.
 
-if not os.path.exists("/skrepo"):
-    subprocess.run('git clone https://github.com/scikit-build/scikit-build /skrepo', shell=True, check=True)
-
 if not os.path.exists("/boost"):
     for command in (
         'curl -L https://boostorg.jfrog.io/artifactory/main/release/1.79.0/source/boost_1_79_0.tar.gz -o boost.tar.gz',
