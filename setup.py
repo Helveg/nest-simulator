75a4a4ec3c860293db112c7e0b08d95febe49d76
Robin De Schepper <robin.deschepper93@gmail.com>
2022-11-29 15:08:26 +0100

    Formatted setup.py

--- setup.py
+++ setup.py
@@ -23,42 +23,44 @@ from setuptools import find_packages
 from skbuild import setup
 
 setup(
-    name = 'nest-simulator',
-    version = '3.3.0-dev0',
-    description = 'Python bindings for NEST',
-    author = 'The NEST Initiative',
-    url = 'https://www.nest-simulator.org',
-    license = 'GPLv2+',
-    packages = find_packages(where="pynest"),
-    package_dir = {
-        "": "pynest"
-    },
-    install_requires = ['numpy', 'scipy'],
-    extras_require = {'test': ['junitparser', 'matplotlib', 'nose']},
-    classifiers = [
-        'Development Status :: 6 - Mature',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    name="nest-simulator",
+    version="3.4.0-dev0",
+    description="Python bindings for NEST",
+    author="The NEST Initiative",
+    url="https://www.nest-simulator.org",
+    license="GPLv2+",
+    packages=find_packages(where="pynest"),
+    package_dir={"": "pynest"},
+    install_requires=["numpy", "scipy"],
+    extras_require={"test": ["junitparser", "matplotlib", "nose"]},
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
-    python_requires = '>=3.8, <4',
-    keywords = (",".join([
-        'nest',
-        'simulator',
-        'neuroscience',
-        'neural',
-        'neuron',
-        'network',
-        'ai',
-        'spike',
-        'spiking'
-    ])),
-    project_urls = {
-        'Homepage': 'https://www.nest-simulator.org/',
-        'Bug Reports': 'https://github.com/nest/nest-simulator/issues',
-        'Source': 'https://github.com/nest/nest-simulator',
-        'Documentation': 'https://nest-simulator.readthedocs.io/'
+    python_requires=">=3.8, <4",
+    keywords=(
+        ",".join(
+            [
+                "nest",
+                "simulator",
+                "neuroscience",
+                "neural",
+                "neuron",
+                "network",
+                "ai",
+                "spike",
+                "spiking",
+            ]
+        )
+    ),
+    project_urls={
+        "Homepage": "https://www.nest-simulator.org/",
+        "Bug Reports": "https://github.com/nest/nest-simulator/issues",
+        "Source": "https://github.com/nest/nest-simulator",
+        "Documentation": "https://nest-simulator.readthedocs.io/",
     },
 )
